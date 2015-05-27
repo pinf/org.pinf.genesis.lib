b3fdf159aa09b3b2f63f1cc09ec297f65b34e444
Christoph Dorn <christoph@christophdorn.com>
2015-05-27 11:00:27 -0700

    wip

--- api/config.js
+++ api/config.js
@@ -6,6 +6,10 @@ const LOCATOR = require("org.pinf.lib/lib/locator");
 require('../lib/api').forModule(require, module, function (API, exports) {
 	// Runs multiple times as module is mapped to other modules.
 
+	if (!API.programDescriptor) {
+		return {};
+	}
+
 	API.ASSERT(typeof API.args['org.pinf.genesis.lib/0/config'].descriptor, "object");
 
 	var descriptor = API.args['org.pinf.genesis.lib/0/config'].descriptor;
@@ -34,10 +38,10 @@ require('../lib/api').forModule(require, module, function (API, exports) {
 
 	function getProgramDescriptor (callback) {
 		if (!process.env.PINF_PROGRAM_PATH) {
-			API.console.verbose("Skip loading (not found) program descriptor from 'PINF_PROGRAM_PATH':", process.env.PINF_PROGRAM_PATH);
+			API.console.verbose("Skip loading (not found) program descriptor from 'PINF_PROGRAM_PATH': " + process.env.PINF_PROGRAM_PATH);
 			return callback(null, null);
 		}
-		API.console.verbose("Loading program descriptor from 'PINF_PROGRAM_PATH':", process.env.PINF_PROGRAM_PATH);
+		API.console.verbose("Loading program descriptor from 'PINF_PROGRAM_PATH': " + process.env.PINF_PROGRAM_PATH);
 		return PROGRAM.fromFile({
 			// TODO: Provide necessary API functions.
 		}, process.env.PINF_PROGRAM_PATH, callback);
@@ -47,7 +51,8 @@ require('../lib/api').forModule(require, module, function (API, exports) {
 		if (err) return callback(err);
 
 		if (!programDescriptor) {
-			return callback(new Error("Error loading program descriptor from 'PINF_PROGRAM_PATH':", process.env.PINF_PROGRAM_PATH));
+			API.console.warn("Warning: Error loading program descriptor from 'PINF_PROGRAM_PATH': " + process.env.PINF_PROGRAM_PATH);
+			return callback(null, {});
 		}
 
 		return callback(null, {
